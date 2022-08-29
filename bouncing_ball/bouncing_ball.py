2903e0a584a7efaf16864aad1bdde67f112b1844
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2022-08-29 08:42:19 +0200

    Capitalised constants

--- bouncing_ball/bouncing_ball.py
+++ bouncing_ball/bouncing_ball.py
@@ -4,15 +4,15 @@ import pygame
 # Initialises modules from pygame
 pygame.init()
 
-width, height = 320, 240
-size = width, height
-speed = [1, 1]
-black = 0, 0, 0
+WIDTH, HEIGHT = 320, 240
+SIZE = WIDTH, HEIGHT
+SPEED = [1, 1]
+BLACK = 0, 0, 0
 
 # Create window
 # Pygame represents images as Surface objects
 # set_mode() creates new Surface that represents actual displayed graphics
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(SIZE)
 
 # Load image
 # Supports bmp, jpg, png, tga, and gif
@@ -31,15 +31,15 @@ while 1:
             sys.exit()
     
     # Move ball
-    ball_rect = ball_rect.move(speed)
+    ball_rect = ball_rect.move(SPEED)
     # Check that ball remains within bounds of window
-    if ball_rect.left < 0 or ball_rect.right > width:
-        speed[0] = -speed[0]
-    if ball_rect.top < 0 or ball_rect.bottom > height:
-        speed[1] = -speed[1]
+    if ball_rect.left < 0 or ball_rect.right > WIDTH:
+        SPEED[0] = -SPEED[0]
+    if ball_rect.top < 0 or ball_rect.bottom > HEIGHT:
+        SPEED[1] = -SPEED[1]
 
-    # Erase screen by covering in black. This gets rid of "old ball"
-    screen.fill(black)
+    # Erase screen by covering in BLACK. This gets rid of "old ball"
+    screen.fill(BLACK)
     # Draw "new ball"
     # blit refers to copying pixel colour from one image to another
     # blit() needs a surface to copy, and a postition on which to paste
