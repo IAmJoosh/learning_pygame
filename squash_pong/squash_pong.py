033ecd631a157484e3c746200f69198ca83be9e3
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2022-08-30 14:17:40 +0200

    Realised can't use .get_rect(). Swapping to OOP

--- squash_pong/squash_pong.py
+++ squash_pong/squash_pong.py
@@ -7,10 +7,11 @@ FG_COLOUR = pygame.Color("white")
 BG_COLOUR = pygame.Color("black")
 WIDTH, HEIGHT = 1200, 800
 SCREEN_SIZE = WIDTH, HEIGHT
-BALL_SPEED = [2, 2]
-BORDER_WIDTH = 25
+BALL_SPEED = [-2, 2]
+BORDER_WIDTH = PADDLE_WIDTH = 25
+PADDLE_LENGTH = (HEIGHT - 2 * BORDER_WIDTH) * 0.25
 BALL_RADIUS = 30
-PADDING = 50
+PADDING = 10
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Squash Pong")
@@ -18,8 +19,16 @@ screen.fill(BG_COLOUR)
 
 ball = pygame.draw.circle(screen, 
                             FG_COLOUR, (
-                            WIDTH - BALL_RADIUS - PADDING, HEIGHT/2), 
+                            WIDTH - PADDING - PADDLE_WIDTH - BALL_RADIUS, 
+                            HEIGHT/2), 
                             BALL_RADIUS)
+
+paddle = pygame.draw.rect(screen,
+                            FG_COLOUR,
+                            (WIDTH - PADDLE_WIDTH - PADDING,
+                            HEIGHT/2 - PADDLE_LENGTH/2,
+                            PADDLE_WIDTH,
+                            PADDLE_LENGTH))
 
 pygame.draw.rect(screen,
                     FG_COLOUR,
@@ -38,5 +47,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    
+    ball = ball.move(BALL_SPEED)
+    if ball.left < BORDER_WIDTH or ball.right > WIDTH:
+        BALL_SPEED[0] = -BALL_SPEED[0]
+    if ball.top < BORDER_WIDTH or ball.bottom > (HEIGHT - BORDER_WIDTH):
+        BALL_SPEED[1] = -BALL_SPEED[1]
+    
+    screen.fill(BG_COLOUR)
+    screen.blit(ball, ball_rect)
     pygame.display.flip()
